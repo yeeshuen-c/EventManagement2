7b99db870d3de301aecdb8eaa99fa6f39ca442ff
Anyee27 <91144543+yeeshuen-c@users.noreply.github.com>
2025-03-22 14:29:49 +0800

    Update testmongo.js

--- backend/testmongo.js
+++ backend/testmongo.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://admin:admin27@cluster0.7jngb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://admin:.........";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
